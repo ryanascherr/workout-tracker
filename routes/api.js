84303c530747c1b5c9f42b711b5ce75f07195669
ryanascherr <ryanascherr@gmail.com>
2021-06-26 09:48:14 -0400

    Seeded data, modularized model export.

--- routes/api.js
+++ routes/api.js
@@ -23,15 +23,15 @@ router.get("/api/workouts/range", (req, res) => {
       });
   });
 
-router.put("/api/workouts/" + id, ({ body }, res) => {
-  Workout.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+// router.put("/api/workouts/" + id, ({ body }, res) => {
+//   Workout.create(body)
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.insertMany(body)
