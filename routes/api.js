dfb5dc9c2c379d79d4a3a440458b1bca8830fade
ryanascherr <ryanascherr@gmail.com>
2021-06-29 10:42:15 -0400

    Began working on stats page route.

--- routes/api.js
+++ routes/api.js
@@ -25,6 +25,22 @@ router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.get("/stats", (req, res) => {
+  Workout.aggregate([
+    {
+      addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration"
+        },
+        totalWeight: {
+          $sum: "$exercises.weight"
+        },
+      }
+    }
+  ])
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 //aggregate
 // router.get("/api/workouts/range", (req, res) => {
 //     Workout.find({})
